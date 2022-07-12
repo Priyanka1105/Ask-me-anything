b1c6150b7bc2e6c4ea85fab222dc7d2c0009f8e9
Khandelwal <p.c.khandelwal@accenture.com>
2022-07-12 17:12:38 +0530

    master repo

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.teal,
+          centerTitle: true,
+        ),
+        body: MagicBallPage(),
+      ),
+    );
+  }
+}
+
+class MagicBallPage extends StatefulWidget {
+  const MagicBallPage({Key? key}) : super(key: key);
+
+  @override
+  State<MagicBallPage> createState() => _MagicBallPageState();
+}
+
+class _MagicBallPageState extends State<MagicBallPage> {
+  var imageNum = 1;
+  void changeImage(){
+    setState((){
+      imageNum = Random().nextInt(5)+1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                changeImage();
+                print('Image is clicked');
+                print(imageNum);
+              },
+              child: Image.asset('images/ball$imageNum.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
